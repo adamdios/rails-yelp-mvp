4c228c3de1ceab216fbedab81640283820bf5037
Adam Dios <adam.dios@gmail.com>
2019-11-06 13:03:43 +0100

    Restaurant model done, validations done, seeding done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts "Destroying all of the Restaurants"
+Restaurant.destroy_all
+
+10.times do
+  Restaurant.create(name: Faker::FunnyName.name , address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.phone_number, category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+end
