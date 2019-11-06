549e618d8e1db0fe421a9377e6718cce68a406d3
Adam Dios <adam.dios@gmail.com>
2019-11-06 16:32:52 +0100

    Restaurant COntroller and Reviews Controller done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get "restaurants", to: "restaurants#index"
+  # get "restaurants/:restaurant_id/reviews", to: "reviews#index"
+
+  # get "restaurants/new", to: "restaurants#new", as: :new
+  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
+
+  # post "restaurants", to: "tasks#create"
+
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [ :new, :create ]
+  end
+
+  # get "restaurants/:restaurant_id",
 end
+
